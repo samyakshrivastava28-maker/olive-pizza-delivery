11fe5178603ceaaac5b6eb25c4872901a075db3a
samyakshrivastava28-maker <samyakshrivastava28@gmail.com>
2026-09-14 12:28:39 +0530

    fix(build): enforce strict mobile/tablet scope and remove unrequested desktop artifacts

--- electron/sign.cjs
+++
@@ -1,6 +0,0 @@
-﻿// Unsigned Demo Build Signer Hook for electron-builder
-exports.default = async function() {
-  // Unsigned demo installer
-  return Promise.resolve();
-};
-module.exports = exports.default;
